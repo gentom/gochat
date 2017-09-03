4e0cb56cdc05cf70aad01c42578215454ef5d582
gentom <gento.m.developer@gmail.com>
2017-09-03 23:34:03 +0900

    commit

--- client.go
+++ client.go
@@ -20,3 +20,10 @@ func (c *client) read() {
 	}
 	c.socket.Close()
 }
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+}
