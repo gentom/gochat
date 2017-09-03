cfd4a1b79dac599be518760dd853402b5d5e86df
gentom <gento.m.developer@gmail.com>
2017-09-03 23:31:41 +0900

    commit

---
+++ client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
