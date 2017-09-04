2ba9f44d65035eb6bda7eedf2ef82157dac493bb
gentom <gento.m.developer@gmail.com>
2017-09-04 21:39:18 +0900

    commit

--- room.go
+++ room.go
@@ -1,4 +1,22 @@
 package main
+
 type room struct {
 	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			//join
+			r.clients[client] = true
+		case client := <-r.leave:
+			//leave
+			delete(r.clients, client)
+			close(client.send)
+		}
+	}
 }
